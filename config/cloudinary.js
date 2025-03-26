b4be8a11aed1a4d4a7165c4788c5e5943d485853
Alice Mukarwema <157026426+alicemukarwema@users.noreply.github.com>
2025-03-27 01:01:31 +0200

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME ||"dbgvwi4lv",
+  api_key: process.env.CLOUDINARY_API_KEY||"732377679595398",
+  api_secret: process.env.CLOUDINARY_API_SECRET||"oyLob6uNKDjus5pwqff-x6Qgmxw",
 });
 
 export default cloudinary;
