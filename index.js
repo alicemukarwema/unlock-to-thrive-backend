35abd14fb2a83a0eab5bfe5e55b4271ed177eb96
Alice Mukarwema <157026426+alicemukarwema@users.noreply.github.com>
2025-04-02 21:16:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'https://unlocktothrive.netlify.app/',
+  origin: process.env.FRONTEND_URL || 'https://unlocktothrive.netlify.app',
   credentials: true
 }));
 app.use(express.json());
